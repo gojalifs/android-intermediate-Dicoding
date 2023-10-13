13287627e517e8fba6d9aedb52eec2ca4b21f1ba
gojalifs <fajarsidik1999@gmail.com>
2023-10-13 16:54:27 +0700

    added foreground service

--- ServiceApp/app/src/main/java/com/satria/dicoding/latihan/serviceapp/MainActivity.kt
+++ ServiceApp/app/src/main/java/com/satria/dicoding/latihan/serviceapp/MainActivity.kt
@@ -1,29 +1,60 @@
 package com.satria.dicoding.latihan.serviceapp
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.satria.dicoding.latihan.serviceapp.databinding.ActivityMainBinding
 import com.satria.dicoding.latihan.serviceapp.services.MyBackgroundService
+import com.satria.dicoding.latihan.serviceapp.services.MyForegroundService
+import android.Manifest
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStartBackgroundService.setOnClickListener(this)
-        binding.btnStopBackgroundService.setOnClickListener(this)
-    }
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
-    override fun onClick(v: View) {
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
-        when (v) {
-            binding.btnStartBackgroundService -> startService(serviceIntent)
-            binding.btnStopBackgroundService -> stopService(serviceIntent)
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+
+
+        binding.btnStartBackgroundService.setOnClickListener {
+
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener { stopService(serviceIntent) }
+        binding.btnStartForegroundService.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
     }
+
+
 }
