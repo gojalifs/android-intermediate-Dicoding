5e17d779b4986ac68a9313bebe74b1fd554a5ca0
gojali99 <fajarsidik1999@gmail.com>
2023-10-25 15:53:03 +0700

    geofence app

---
+++ GeofenceApp/app/src/main/java/com/fas/dev/latihan/geofenceapp/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,79 @@
+package com.fas.dev.latihan.geofenceapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        if (intent.action == ACTION_GEOFENCE_EVENT) {
+            val geofencingEvent = GeofencingEvent.fromIntent(intent) ?: return
+
+            if (geofencingEvent.hasError()) {
+                val errorMessage =
+                    GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
+                Log.e(TAG, "onReceive: $errorMessage")
+                sendNotification(context, errorMessage)
+                return
+            }
+
+            val geofenceTransition = geofencingEvent.geofenceTransition
+
+            if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+                val geofenceTransitionString = when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> "Anda Telah Memasuki Area"
+                    Geofence.GEOFENCE_TRANSITION_DWELL -> "Anda Telah Di Dalam Area"
+                    else -> "Invalid transition type"
+                }
+                val triggeringGeofences = geofencingEvent.triggeringGeofences
+                triggeringGeofences?.forEach {
+                    val geofenceTransitionDetails = "$geofenceTransitionString ${it.requestId}"
+                    Log.i(TAG, "onReceive: $geofenceTransitionDetails")
+                    sendNotification(context, geofenceTransitionDetails)
+                }
+            } else {
+                val errorMessage = "Invalid transition type: $geofenceTransition"
+                Log.e(TAG, "onReceive: $errorMessage")
+                sendNotification(context, errorMessage)
+            }
+        }
+    }
+
+    private fun sendNotification(context: Context, geofenceTransitionDetails: String) {
+        val mNotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(geofenceTransitionDetails)
+            .setContentText("Anda sudah bisa absen sekarang :)")
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            mBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        private const val TAG = "GeofenceBroadcast"
+        const val ACTION_GEOFENCE_EVENT = "GeofenceEvent"
+
+        private const val CHANNEL_ID = "1"
+        private const val CHANNEL_NAME = "Geofence Channel"
+        private const val NOTIFICATION_ID = 1
+    }
+}
